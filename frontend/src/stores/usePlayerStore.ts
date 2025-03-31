f12ddd8544deb44408caff3e15333f6b22f471be
LOHAN2000 <anghelovy@outlook.com>
2025-03-30 23:04:47 -0500

    Agregar lógica para manejar la cola de reproducción; implementar funciones para inicializar la cola, reproducir álbumes, establecer la canción actual y navegar entre canciones

--- frontend/src/stores/usePlayerStore.ts
+++ frontend/src/stores/usePlayerStore.ts
@@ -17,9 +17,59 @@ interface PlayerStore {
 }
 
 export const usePlayerStore = create<PlayerStore>((set, get) => ({
-  currentSong; null,
+  currentSong: null,
   isPlaying: false,
   queue: [],
   currentIndex: 0,
 
+  initializeQueue: async (songs) => {
+    set({ queue: songs, currentSong: get().currentSong || songs [0], currentIndex: get().currentIndex === -1 ? 0 : get().currentIndex})
+  },
+
+  playAlbum: async (songs, starIndex = 0) => {
+    if(songs.length === 0) {
+      return;
+    }
+    const song = songs[starIndex];
+    set({ queue: songs, currentSong: song, currentIndex: starIndex, isPlaying: true });
+  },
+
+  setCurrentSong: async (song = null) => {
+    if (!song) {
+      return;
+    }
+
+    const songIndex = get().queue.findIndex(s => s.song_id === song.song_id);
+    
+    set({ currentSong: song, isPlaying: true, currentIndex: songIndex !== -1 ? songIndex : get().currentIndex });
+  },
+
+  togglePlay: async () => {
+    const willStartPlaying = !get().isPlaying;
+    set({ isPlaying: willStartPlaying })
+  },
+
+  playNext: async () => {
+    const { currentIndex, queue } = get();
+    const nextIndex = currentIndex + 1;
+
+    if (nextIndex < queue.length) {
+      const nextSong = queue[nextIndex];
+      set({ currentSong: nextSong, currentIndex:nextIndex, isPlaying: true })
+    } else (
+      set({ isPlaying: false })
+    )
+  },
+
+  playPrevious: async () => {
+    const { currentIndex, queue } = get();
+    const prevIndex = currentIndex - 1;
+
+    if(prevIndex >= 0) {
+      const prevSong = queue[prevIndex];
+      set({ currentSong: prevSong, currentIndex: prevIndex, isPlaying: true });
+    } else {
+      set({ isPlaying: false });
+    } 
+  }
 }))
