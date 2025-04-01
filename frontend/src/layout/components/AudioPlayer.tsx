ae4ac88d81ef803b55f7b938804fbf5e7654f536
LOHAN2000 <anghelovy@outlook.com>
2025-03-31 23:26:55 -0500

    Actualizar lógica de AudioPlayer; agregar efectos para pausar/reproducir y manejar cambios de canción

--- frontend/src/layout/components/AudioPlayer.tsx
+++ frontend/src/layout/components/AudioPlayer.tsx
@@ -8,10 +8,15 @@ export const AudioPlayer = () => {
 
   const { currentSong, isPlaying, playNext } = usePlayerStore();
 
+  // useEffect para pausar o reproducir cancion
+
   useEffect(() => {
     if(isPlaying) audioRef.current?.play();
     else audioRef.current?.pause();
   }, [isPlaying])
+
+
+  // useEffect para cambiar la cancion mediante playNext
 
   useEffect(() => {
     const audio = audioRef.current;
@@ -24,16 +29,30 @@ export const AudioPlayer = () => {
     return () => audio?.removeEventListener('ended', handleEnded)
   }, [playNext])
 
+
+  // useEffect para actualizar la cancion 
+
   useEffect(() => {
-    if(!audioRef.current || !currentSong) return;
+    if (!audioRef.current || !currentSong) return;
     const audio = audioRef.current;
-
-    const isSongChange = prevSongRef.current !== currentSong?.audio_Url;
-
-    if(isSongChange) {
-      audio.src = currentSong?.audio_Url
+    const isSongChange = prevSongRef.current !== currentSong.audio_Url;
+  
+    if (isSongChange) {
+      // Si la canción cambió, se actualiza el src y se reinicia
+      audio.src = currentSong.audio_Url;
+      audio.currentTime = 0;
+      prevSongRef.current = currentSong.audio_Url;
+      if (isPlaying) {
+        audio.play();
+      }
+    } else {
+      // Si es la misma canción, reiniciamos el tiempo y forzamos el play
+      audio.currentTime = 0;
+      if (isPlaying) {
+        audio.play();  // Se asegura que vuelva a reproducirse
+      }
     }
-  }, [])
+  }, [currentSong, isPlaying]);
 
 
   return <audio ref={audioRef}/>
