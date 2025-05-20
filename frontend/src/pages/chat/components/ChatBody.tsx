9c781cdd3c339feb31a57ebdfad6a371edaf4290
LOHAN2000 <anghelovy@outlook.com>
2025-05-20 18:07:16 -0500

    Refactor estructura de mensajes en el componente ChatBody; mejorar la presentación de mensajes y la función de formato de fecha.

--- frontend/src/pages/chat/components/ChatBody.tsx
+++ frontend/src/pages/chat/components/ChatBody.tsx
@@ -2,6 +2,15 @@ import { useChatStore } from '@/stores/useChatStore'
 import { useUser } from '@clerk/clerk-react';
 import { useEffect, useRef } from 'react';
 
+const formatDate = (date: string) => {
+  return new Date(date).toLocaleDateString('en-US', {
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: true
+  })
+}
+
+
 export const ChatBody = () => {
 
   const { user } = useUser();
@@ -21,11 +30,14 @@ export const ChatBody = () => {
       {messages.map((message) => {
         const isMine = message.sender_id === user?.id;
         return (
-          <div key={message.message_id} className={`flex items-center ${isMine ? 'flex-row-reverse space-x-2 space-x-reverse' : 'space-x-2'}`}>
+          <div key={message.message_id} className={`flex flex-col sm:flex-row sm:items-center ${isMine ? 'flex-col sm:jus items-end sm:flex-row-reverse space-x-2 space-y-2 sm:space-y-0 space-x-reverse' : 'space-x-2 space-y-2 sm:space-y-0'}`}>
             <div className='w-8 h-8 rounded-full overflow-hidden'>
               <img src={isMine ? user.imageUrl : selectedUser?.image_Url} className='w-full h-full object-cover'/>
             </div>
-            <h1 className={`text-start  ${isMine ? 'bg-emerald-600' : 'bg-zinc-600'} p-2 rounded-xl max-w-2xl`}>{message.content}</h1>
+            <div className={`flex flex-col ${isMine ? 'items-end' : 'items-start'}`}>
+              <h1 className={`text-start w-fit ${isMine ? 'bg-emerald-600' : 'bg-zinc-600'} p-2 rounded-xl max-w-2xl`}>{message.content}</h1>
+              <h1 className='text-end text-sm sm:text-md text-zinc-500'>{formatDate(message.created_at)}</h1>
+            </div>
           </div>
         );
       })}
